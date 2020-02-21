2dbf04ce944e20d22277916f4e4a354175e70472
AndrewRoss-at-973766358289 <l22ns5lB0OqsmGZaJwMnCB1LbrkyjfcwRftwN5qC0+Y=>
2020-02-21 14:16:53 +0200

    -

---
+++ Api/Controllers/TestyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TestyController : ControllerBase
+	{
+		[HttpGet("GetName/{name}")]
+		public String GetName(String name)
+		{
+			return name;
+		}
+	}
+}
